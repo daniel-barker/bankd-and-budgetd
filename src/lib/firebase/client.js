a133a85e909a8e0d25b96fd221b6a6eb292d3cc0
dan barker <dbarker16@gmail.com>
2025-04-01 17:56:30 -0400

    yes i do

--- src/lib/firebase/client.js
+++ src/lib/firebase/client.js
@@ -1,28 +1,28 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { browser } from "$app/environment";
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { browser } from '$app/environment';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDjQs85_4unkYFRZuZKg8nNb-0lQ-noBew",
-  authDomain: "bankd-and-budgetd.firebaseapp.com",
-  projectId: "bankd-and-budgetd",
-  storageBucket: "bankd-and-budgetd.firebasestorage.app",
-  messagingSenderId: "1046678297149",
-  appId: "1:1046678297149:web:2caa14f2b03ed27de7a854",
-  measurementId: "G-GG2YMGDQNY"
+	apiKey: 'AIzaSyDjQs85_4unkYFRZuZKg8nNb-0lQ-noBew',
+	authDomain: 'bankd-and-budgetd.firebaseapp.com',
+	projectId: 'bankd-and-budgetd',
+	storageBucket: 'bankd-and-budgetd.firebasestorage.app',
+	messagingSenderId: '1046678297149',
+	appId: '1:1046678297149:web:2caa14f2b03ed27de7a854',
+	measurementId: 'G-GG2YMGDQNY'
 };
 
 // Initialize Firebase
-const app = browser ? initializeApp(firebaseConfig) : null;
-const analytics = app ? getAnalytics(app) : null;
-const auth = app ? getAuth(app) : null ;
-const db = app ? getFirestore(app) : null;
+const app = browser ? initializeApp(firebaseConfig) : initializeApp(firebaseConfig);
+const analytics = browser ? getAnalytics(app) : null;
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 export { app, analytics, auth, db };
